b1c92b4d86655c250b7908ad924556f1840bae0e
pateldd1 <devanshpatel22@gmail.com>
2023-03-11 00:26:11 -0500

    commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 const PG_CONFIG = {
   user: process.env.DB_USER,
@@ -6,15 +6,15 @@ const PG_CONFIG = {
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: 5432,
-}
+};
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
+app.get("/", function (req, res) {
+  res.send("Hello world!");
 });
 
-app.get('/db_healthcheck', async function(_, res) {
-  const { Pool } = require('pg')
-  const pool = new Pool(PG_CONFIG)
+app.get("/db_healthcheck", async function (_, res) {
+  const { Pool } = require("pg");
+  const pool = new Pool(PG_CONFIG);
   const text = `
     CREATE TABLE IF NOT EXISTS "users" (
 	    "id" SERIAL,
@@ -23,17 +23,18 @@ app.get('/db_healthcheck', async function(_, res) {
 	    PRIMARY KEY ("id")
     );`;
   try {
-    await pool.query(text)
+    await pool.query(text);
   } catch (error) {
-    console.log('already created')
-    console.log(error)
+    console.log("already created");
+    console.log(error);
   }
 
-  const d_res = await pool.query('SELECT * FROM users LIMIT 5')
-  pool.end()
-  return res.send(d_res)
-})
+  const d_res = await pool.query("SELECT * FROM users LIMIT 5");
+  pool.end();
+  return res.send(d_res);
+});
 
 app.listen(3000, "0.0.0.0", 10, () => {
-  console.log('app is running in `http://localhost:3000/`...');
+  console.log("app is running in `http://localhost:3000/`...");
 });
+//
