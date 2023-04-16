1590b592beeb9b3b00b58ee691a99021c8d029cf
pateldd1 <devanshpatel22@gmail.com>
2023-04-16 03:26:26 -0400

    continue

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -7,6 +7,10 @@ locals {
   azs          = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
+data "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.this.id
+}
+
 resource "aws_vpc" "this" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -118,7 +122,7 @@ resource "aws_route_table_association" "private" {
 resource "aws_security_group" "bastion" {
   name        = "ECS-Bastion-SG"
   description = "Security group for the bastion host"
-  vpc_id      = aws_vpc
+  vpc_id      = aws_vpc.this.id
   type        = "ingress"
   from_port   = 22
   to_port     = 22
@@ -127,7 +131,7 @@ resource "aws_security_group" "bastion" {
 }
 
 resource "aws_security_group_rule" "bastion_egress" {
-  security_group_id = aws_security_group.bastion.id
+  security_group_id = data.aws_default_security_group.default.id
 
   type        = "egress"
   from_port   = 0
@@ -143,11 +147,20 @@ resource "aws_instance" "bastion" {
   subnet_id = aws_subnet.public[0].id
   key_name  = "your-key-pair-name" # Replace with your key pair name
 
-  vpc_security_group_ids = [aws_security_group.bastion.id]
+  vpc_security_group_ids = [data.aws_default_security_group.default.id]
 
   tags = {
     Name = "ECS-Bastion-Host"
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y python3-pip
+              pip3 install ansible
+              aws s3 cp s3://your-bucket-name/crowdstrike_install.yml /tmp/crowdstrike_install.yml
+              ansible-playbook /tmp/crowdstrike_install.yml
+              EOF
 }
 
 resource "aws_security_group" "ecs_tasks" {
@@ -164,17 +177,96 @@ resource "aws_ecs_cluster" "this" {
   name = local.cluster_name
 }
 
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_execution_role.name
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+# Allow the ECS task to talk to RDS.
+resource "aws_iam_role_policy_attachment" "ecs_task_role_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+  role       = aws_iam_role.ecs_task_role.name
+}
+
+resource "aws_iam_role_policy" "ecs_task_role_policy" {
+  name = "ecs_task_role_policy"
+  role = aws_iam_role.ecs_task_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+variable "my_secret_value" {
+  default = ""
+}
+
+resource "aws_secretsmanager_secret" "my_secret" {
+  name        = "my_secret"
+  description = "This is a secret for demonstration purposes"
+}
+
+# pass in the secret at runtime when doing terraform apply
+resource "aws_secretsmanager_secret_version" "my_secret_version" {
+  secret_id     = aws_secretsmanager_secret.my_secret.id
+  secret_string = lookup(var.my_secret_value, "")
+}
+
 resource "aws_ecs_task_definition" "this" {
   family                   = "my-ecs-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([
     {
-      name  = "my-container"
-      image = "nginx:latest"
+      name  = "interview-container"
+      image = "429363805278.dkr.ecr.us-east-1.amazonaws.com/interview:latest"
       portMappings = [
         {
           containerPort = 80
@@ -196,7 +288,7 @@ resource "aws_ecs_service" "this" {
 
   network_configuration {
     subnets          = aws_subnet.private.*.id
-    security_groups  = [aws_security_group.ecs_tasks.id]
+    security_groups  = [data.aws_default_security_group.default.id]
     assign_public_ip = false
   }
 
@@ -260,7 +352,7 @@ resource "aws_lb_target_group" "this" {
     unhealthy_threshold = "3"
     timeout             = "5"
     interval            = "30"
-    path                = "/"
+    path                = "/health"
   }
 }
 
@@ -273,4 +365,18 @@ resource "aws_lb_listener" "frontend" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.this.arn
   }
+}
+
+resource "aws_db_instance" "this" {
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "13.4"
+  instance_class         = "db.t2.micro"
+  name                   = "terraform_practice_db"
+  username               = local.secrets.db_username
+  password               = local.secrets.db_password
+  parameter_group_name   = "default.postgres13"
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+  vpc_security_group_ids = [data.aws_default_security_group.default.id]
 }
